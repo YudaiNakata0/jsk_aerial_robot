7243630c4fa48dc1fec557cd3c3e4875d4e29b30
Yudai Nakata <nakata@dragon.t.u-tokyo.ac.jp>
2025-09-12 18:14:49 +0900

    [camera]added line detection

--- camera/src/red_detection.py
+++ camera/src/red_detection.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import cv2
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
 class ImageProcess():
-    def __init__(self, input_img):
+    def __init__(self, input_img, file_name):
         self.img = input_img
+        self.file_name = file_name
         self.lh1 = 0
         self.lh2 = 170
         self.ls = 100
@@ -31,11 +33,47 @@ class ImageProcess():
         masked_img = cv2.bitwise_and(self.img, self.img, mask=self.mask)
         plt.imshow(cv2.cvtColor(masked_img, cv2.COLOR_BGR2RGB))
         plt.show()
-    
 
+    def show_mask(self):
+        plt.imshow(self.mask)
+        plt.show()
+
+    def save_mask(self):
+        new_name = "masked_" + self.file_name
+        cv2.imwrite(new_name, self.mask)
+
+    def get_line(self):
+        #lines = cv2.HoughLinesP(self.mask, rho=1, theta=np.pi/360, threshold=50, minLineLength=50, maxLineGap=5)
+        #print(lines)
+        edges = cv2.Canny(self.mask, 50, 150)
+        lines = cv2.HoughLinesP(edges, 1, np.pi/180, threshold=30, minLineLength=30, maxLineGap=10)
+        print(lines)
+        if lines is not None:
+            for x1, y1, x2, y2 in lines[0]:
+                print(x1, y1, x2, y2)
+                cv2.line(self.mask, (x1, y1), (x2, y2), (255, 0, 0), 4)
+            plt.imshow(self.mask)
+            plt.show()
+
+
+    def draw_line(self):
+        p1 = 100, 100
+        p2 = 300, 300
+        color = 255, 0, 0
+        width = 3
+        cv2.line(self.img, p1, p2, color, width)
+        plt.imshow(self.img)
+        plt.show()
+        
 if __name__ == "__main__":
-    input_img = cv2.imread("test.JPG")
-    Image = ImageProcess(input_img=input_img)
+    args = sys.argv
+    file_name = args[1]
+    input_img = cv2.imread(file_name)
+    Image = ImageProcess(input_img=input_img, file_name=file_name)
     Image.convert_format()
     Image.generate_mask()
-    Image.mask_image()
+    #Image.mask_image()
+    #Image.get_line()
+    #Image.show_mask()
+    Image.draw_line()
+    Image.save_mask()
