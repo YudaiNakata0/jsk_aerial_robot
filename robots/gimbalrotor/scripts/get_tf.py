a83f41a74c0f696d9810d10f9053e1dc9aff8416
Yudai Nakata <nakata@dragon.t.u-tokyo.ac.jp>
2025-11-18 12:30:57 +0900

    [gimbalrotor/scripts](get_tf.py)publisher

--- robots/gimbalrotor/scripts/get_tf.py
+++ robots/gimbalrotor/scripts/get_tf.py
@@ -2,11 +2,13 @@
 #!/usr/bin/env python3
 import rospy
 import tf
+from geometry_msgs.msg import Pose
 
 if __name__ == '__main__':
     rospy.init_node('get_tf_node')
 
     listener = tf.TransformListener()
+    publisher = rospy.Publisher("/gimbalrotor/endeffector_pose", Pose, queue_size=1)
 
     rate = rospy.Rate(1.0)
     while not rospy.is_shutdown():
@@ -15,6 +17,16 @@ if __name__ == '__main__':
             rospy.loginfo("Translation: %s", trans)
             rospy.loginfo("Rotation (quaternion): %s", rot)
             print("----------------------------------")
+            msg = Pose()
+            msg.position.x = trans[0]
+            msg.position.y = trans[1]
+            msg.position.z = trans[2]
+            msg.orientation.x = rot[0]
+            msg.orientation.y = rot[1]
+            msg.orientation.z = rot[2]
+            msg.orientation.w = rot[3]
+            publisher.publish(msg)
+            
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
         rate.sleep()
