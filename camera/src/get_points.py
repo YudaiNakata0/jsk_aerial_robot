8daa2a0e2710357dc8a63c1225b62b7c26880a2f
Yudai Nakata <nakata@dragon.t.u-tokyo.ac.jp>
2025-11-19 09:01:51 +0900

    [camera](get_points.py) publish position of target

--- camera/src/get_points.py
+++ camera/src/get_points.py
@@ -4,17 +4,20 @@ from cv_bridge import CvBridge
 import rospy
 import numpy as np
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import Vector3
 
 class GetTarget():
     def __init__(self):
         self.bridge = CvBridge()
         self.points = []
+        self.number_of_points = 0
         self.image_raw = Image()
         self.setup_ros()
 
     def setup_ros(self):
         self.sub_mask = rospy.Subscriber("/processed_image/filtered_mask", Image, self.callback)
-        self.pub_center = rospy.Publisher("/processed_image/center", Image, queue_size=1)
+        self.pub_center_image = rospy.Publisher("/processed_image/center", Image, queue_size=1)
+        self.pub_center_coords = rospy.Publisher("/target/2D_position", Vector3, queue_size=1)
 
     def callback(self, msg):
         self.get_image(msg)
@@ -28,14 +31,23 @@ class GetTarget():
         self.mono = self.bridge.imgmsg_to_cv2(self.image, desired_encoding="mono8")
         ys, xs = np.where(self.mono == 255)
         self.points = list(zip(xs, ys))
+        self.number_of_points = len(self.points)
+        print(self.number_of_points)
 
     def calculate_center(self):
+        if self.number_of_points < 1000:
+            print("No sufficient points.")
+            return
         center = np.mean(self.points, axis=0)
         print(center)
         center_image = cv2.cvtColor(self.mono, cv2.COLOR_GRAY2RGB)
         cv2.circle(center_image, (int(center[0]), int(center[1])), 5, (0, 255, 0), -1)
         pub_msg = self.bridge.cv2_to_imgmsg(center_image, encoding="bgr8")
-        self.pub_center.publish(pub_msg)
+        self.pub_center_image.publish(pub_msg)
+        pub_coords_msg = Vector3()
+        pub_coords_msg.x = center[0]
+        pub_coords_msg.y = center[1]
+        self.pub_center_coords.publish(pub_coords_msg)
     
 if __name__ == "__main__":
     rospy.init_node("detect_target_node")
