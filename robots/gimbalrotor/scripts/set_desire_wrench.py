1b4bf882e4e72990a87a53e83889fd9ff9a06496
Yudai Nakata <nakata@dragon.t.u-tokyo.ac.jp>
2025-12-05 00:54:15 +0900

    [gimbalrotor](set_desire_wrench.py) x,y,z control mode switch

--- robots/gimbalrotor/scripts/set_desire_wrench.py
+++ robots/gimbalrotor/scripts/set_desire_wrench.py
@@ -6,7 +6,7 @@ import tty
 import select
 import time
 from geometry_msgs.msg import WrenchStamped
-from std_msgs.msg import Bool
+from std_msgs.msg import Bool, Int8
 
 msg = """
 x: enter setting x wrench mode
@@ -16,9 +16,10 @@ q: reset mode
 [: +0.1
 ]: -0.1
 p: publish
-@: publish flags (true)
+@: publish flags (true, pos->vel)
 ,: publish send_feedforward_switch_flag (false)
 .: publish attaching_flag (false)
+/: publish flags (false, vel->pos)
 """
 
 def get_key():
@@ -41,7 +42,10 @@ def main():
     pub_flag_1 = rospy.Publisher("/gimbalrotor/send_feedforward_switch_flag", Bool, queue_size=1)
     pub_flag_2 = rospy.Publisher("/gimbalrotor/attaching_flag", Bool, queue_size=1)
     pub_flag_3 = rospy.Publisher("/gimbalrotor/xyz_wrench_control_flag", Bool, queue_size=1)
-
+    pub_x_control_mode = rospy.Publisher("/gimbalrotor/teleop_command/x_ctrl_mode", Int8, queue_size=1)
+    pub_y_control_mode = rospy.Publisher("/gimbalrotor/teleop_command/y_ctrl_mode", Int8, queue_size=1)
+    pub_z_control_mode = rospy.Publisher("/gimbalrotor/teleop_command/z_ctrl_mode", Int8, queue_size=1)
+    
     print(msg)
     print("waiting key input... (Ctrl-C to quit)")
 
@@ -50,6 +54,9 @@ def main():
     msg_f1 = Bool()
     msg_f2 = Bool()
     msg_f3 = Bool()
+    msg_x_ctrl = Int8()
+    msg_y_ctrl = Int8()
+    msg_z_ctrl = Int8()
 
     rate = rospy.Rate(50)
 
@@ -66,14 +73,27 @@ def main():
                 break
 
             if key == "@":
-                msg_f1.data = True
-                msg_f2.data = True
-                msg_f3.data = True
-                # pub_flag_1.publish(msg_f1)
-                # pub_flag_2.publish(msg_f2)
-                # print("publish: send_feedforward_switch_flag, attaching_flag")
-                pub_flag_3.publish(msg_f3)
-                print("publish: xyz_wrench_control_flag")
+                if mode == 0:
+                    msg_f1.data = True
+                    msg_f2.data = True
+                    msg_f3.data = True
+                    # pub_flag_1.publish(msg_f1)
+                    # pub_flag_2.publish(msg_f2)
+                    # print("publish: send_feedforward_switch_flag, attaching_flag")
+                    pub_flag_3.publish(msg_f3)
+                    print("publish: xyz_wrench_control_flag")
+                if mode == 1:
+                    msg_x_ctrl.data = 1
+                    pub_x_control_mode.publish(msg_x_ctrl)
+                    print("x control -> vel")
+                if mode == 2:
+                    msg_y_ctrl.data = 1
+                    pub_y_control_mode.publish(msg_y_ctrl)
+                    print("y control -> vel")
+                if mode == 3:
+                    msg_z_ctrl.data = 1
+                    pub_z_control_mode.publish(msg_z_ctrl)
+                    print("z control -> vel")
 
             if key == ",":
                 msg_f1.data = False
@@ -86,10 +106,23 @@ def main():
                 print("publish: attaching_flag->False")
 
             if key == "/":
-                msg_f3.data = False
-                pub_flag_3.publish(msg_f3)
-                print("publish: xyz_wrench_control_flag->False")
-
+                if mode == 0:
+                    msg_f3.data = False
+                    pub_flag_3.publish(msg_f3)
+                    print("publish: xyz_wrench_control_flag->False")
+                if mode == 1:
+                    msg_x_ctrl.data = 0
+                    pub_x_control_mode.publish(msg_x_ctrl)
+                    print("x control -> pos")
+                if mode == 2:
+                    msg_y_ctrl.data = 0
+                    pub_y_control_mode.publish(msg_y_ctrl)
+                    print("y control -> pos")
+                if mode == 3:
+                    msg_z_ctrl.data = 1
+                    pub_z_control_mode.publish(msg_z_ctrl)
+                    print("z control -> pos")
+                    
             if key == "x":
                 mode = 1
                 print("mode -> x")
